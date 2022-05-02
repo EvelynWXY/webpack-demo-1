13174a139774d4fa3290ed813ccbe9e42a6b2f50
Evelyn <ningyuewushang@163.com>
2022-05-02 11:56:14 +0800

    update2

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   //mode:"production" 给用户用的没有任何注释，代码最小化
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   entry: "./src/index.js", //默认入口
   output: {
     filename: "index.[contenthash].js", //dist文件中转译生成的文件名称
